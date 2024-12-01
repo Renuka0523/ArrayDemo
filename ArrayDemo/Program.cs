1b62cd084d2801bcf7ce6d3770f6dc895e14cc8d
Renuka Ajay Chougule <renukachougule.0523@gmail.com>
2024-12-01 23:13:05 +0530

    Add changes

--- ArrayDemo/Program.cs
+++ ArrayDemo/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ArrayDemo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArrayDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var number = new[] { 1, 2, 3, 4, 5, 6 };
+
+            //length
+
+            Console.WriteLine("length of:" + number.Length);
+
+            //Index of
+
+            var index = Array.IndexOf(number, 3);
+            Console.WriteLine("index of:" + index);
+
+            //Clear
+
+            Array.Clear(number, 0, 3);
+            Console.WriteLine("Effect of Clear()");
+
+            foreach (var n in number) 
+            Console.WriteLine(n);
+
+            //Copy
+             int [] another = new int [3];
+            Array.Copy(number, another, 3);
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+                Console.WriteLine(n);
         }
     }
 }
